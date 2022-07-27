93f1ef6b8cee29da069006fcaf86ccb81833f1f4
Otavio Ragazzo <ragazzos08@gmail.com>
2022-07-27 00:34:07 -0300

    Too much progress baby!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,6 +5,23 @@ type Data = {
   name: string
 }
 
+// export const getSpotifySong = async () => {
+//   const response = await fetch(`https://api.spotify.com/v1/search?q=slipknot&type=track`)
+//   const data = await response.json()
+//   return {
+//     props: { data },
+//     revalidate: 60  //In seconds
+//   }
+
+//   // fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+//   //   headers: {
+//   //     Accept: "application/json",
+//   //     Authorization: "Bearer BQB03vu1JqgAxOixr_8plIKvIA53KcV5uE276iIjWNtnrCsGfx8ZUEvhKIWv32nwMuyECkxHUu3rfHxTsQtuNlVrRvXp5I4htLGwwjlcgt2DU8JNJsvVIV1EumBVWLz8SPptcHQnT51hxki78NkDYaDu_GMsFBJl5TJNE5_w471_hf5xSw",
+//   //     "Content-Type": "application/json"
+//   //   }
+//   // })
+// }
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
