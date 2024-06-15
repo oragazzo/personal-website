011c341751c39a3ca004ad31c36fe044a5ec5ce9
Otavio Ragazzo <otavio@65a.com.br>
2024-06-15 13:42:39 -0300

    Adding a better transition and animation to topbar marker

--- components/page-components/TopBarMarker.tsx
+++ components/page-components/TopBarMarker.tsx
@@ -6,6 +6,11 @@ type Props = {
 };
 
 const TopBarMarker: React.FC<Props> = ({ xPos }) => {
+  const calculateDuration = (x: number) => {
+    // Exemplo: duração mínima de 0.5 segundos e máxima de 1 segundo, ajustada pela posição x
+    return Math.min(0.1 + Math.abs(x) / 1000, 0.2);
+  };
+
   return (
     <motion.div
       className={styles.topBarMarker}
@@ -13,6 +18,10 @@ const TopBarMarker: React.FC<Props> = ({ xPos }) => {
         x: xPos - 3,
         rotate: 0,
         opacity: 1,
+      }}
+      transition={{
+        delay: 0.1,
+        duration: calculateDuration(xPos),
       }}
     />
   );
