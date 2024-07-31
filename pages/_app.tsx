81103069a29065d0e3f526a15cfc7a67104e6380
Otavio Ragazzo <otavio@65a.com.br>
2024-07-31 19:23:06 -0300

    + Adding vercel speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.css'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
