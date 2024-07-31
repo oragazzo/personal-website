e5124fea1b9f95f4eade655ebed33b1edae0a8b0
Otavio Ragazzo <otavio@65a.com.br>
2024-07-31 19:27:11 -0300

    + Adding SpeedInsights component to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,5 +5,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  <SpeedInsights />;
   return <Component {...pageProps} />;
 }
